d3e96b980f61a388d4008fcd15525df34ba6c3d3
Andres Quiroz <luisandres.quiroz.contractor>
2021-01-18 21:57:57 -0600

    added files

---
+++ src/test/java/web/taxcoconfig/WebTaxcoConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package web.taxcoconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTaxcoConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
